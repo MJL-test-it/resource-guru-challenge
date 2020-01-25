1d9f7d6fcdb9df8ededa9d9fac4069efc9c61d41
morbidslug <mark.4ndrew.lewis@gmail.com>
2020-01-25 15:43:07 +0000

    added mutating level\! method

--- array_level/lib/array_level.rb
+++ array_level/lib/array_level.rb
@@ -1,3 +1,6 @@
+# Monkey patch of Array class to include custom `flatten` method, `level`
+# do not overwrite flatten to maintain expected performance of Array
+
 class Array
   def level
     self.each_with_object([]) do |elem, ary|
@@ -6,5 +9,9 @@ class Array
       else ary << elem
       end
     end
+  end
+
+  def level!
+
   end
 end
--- array_level/spec/array_level_spec.rb
+++ array_level/spec/array_level_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Array do
   context '#level' do
-    it 'responds to level as an instance method' do
+    it 'responds to `level` as an instance method' do
       expect([]).to respond_to(:level)
     end
     it 'removes a single nest' do
@@ -13,6 +13,11 @@ describe Array do
       expect([[[[]]]].level).to eq []
       expect([1, [2, [3, 4, 5, [6, 7], 8], 9], 10].level).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
       expect([1, [2, [3, 4]], [5, [6, 7], [8], 9], 10].level).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    end
+  end
+  context '#level!' do
+    it 'responds to `level!` as an instance method' do
+      expect([]).to respond_to(:level!)
     end
   end
 end
